73e5214f3a44860b5d24aa266c3e68e15bbf2886
nicolas772 <nicolas.arayau@sansano.usm.cl>
2024-06-18 23:47:31 -0400

    configuracion de eslint y navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import Navbar from "./components/Navbar"
+
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Jet Impresiones
-    </h1>
+    <Navbar></Navbar>
   )
 }
 

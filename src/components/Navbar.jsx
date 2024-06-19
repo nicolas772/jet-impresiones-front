7d7d5c6bb6d2de2d1747d1cbb72bc63e7dedbdba
nicolas772 <nicolas.arayau@sansano.usm.cl>
2024-06-19 19:42:22 -0400

    propios productos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ export default function Navbar () {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className='bg-white'>
+    <header>
       <nav className='mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8' aria-label='Global'>
         <div className='flex lg:flex-1'>
           <a href='#' className='-m-1.5 p-1.5'>
@@ -183,7 +183,8 @@ export default function Navbar () {
                   Quienes Somos
                 </a>
                 <a
-                  href='#'
+                  href='https://www.instagram.com/jet.impresiones3d/'
+                  target='_blank'
                   className='-mx-3 flex gap-2 items-center rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
                 >
                   <InstagramIcon />
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,19 +3,37 @@ import Product from './Product'
 const products = [
   {
     id: 1,
-    name: 'Basic Tee',
+    name: 'Porta completo animales',
     href: '#',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Black'
+    imageSrc: './products/foto1.png',
+    imageAlt: 'Porta completos de distintos animales',
+    price: '$10.000',
+    color: 'Varios colores'
+  },
+  {
+    id: 2,
+    name: 'Macetero mediano',
+    href: '#',
+    imageSrc: './products/foto2.png',
+    imageAlt: 'Maceteros de distintos colores',
+    price: '$15.000',
+    color: 'Varios colores'
+  },
+  {
+    id: 3,
+    name: 'Figura de acción personalizada',
+    href: '#',
+    imageSrc: './products/foto3.png',
+    imageAlt: 'Figura personalizada de accion',
+    price: '$7.000',
+    color: 'Blanco | Amarillo | Negro'
   }
 ]
 
 export default function Products () {
   return (
     <div>
-      <div className='mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8'>
+      <div className='mx-auto max-w-2xl px-4 py-10 sm:px-6 sm:py-10 lg:max-w-7xl lg:px-8'>
         <h2 className='text-2xl font-bold tracking-tight text-gray-900'>Productos disponibles</h2>
         <div className='mt-6 grid gird-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
           {

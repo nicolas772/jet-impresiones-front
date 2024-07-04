e8204798b142b8020858f23973d9e746331c09bc
nicolas772 <nicolas.arayau@sansano.usm.cl>
2024-07-04 19:59:32 -0400

    correccion de estilos a products y mensaje en about Us

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,7 +3,8 @@ import ProductItem from './ProductItem'
 export default function Products ({ products }) {
   return (
     <div id='productos' className='bg-white/95 m-2 rounded-lg'>
-      <div className='px-4 py-4 sm:px-6 sm:py-4 lg:px-8'>
+      <div className='mx-auto max-w-7xl px-4 py-4 sm:px-6 sm:py-4 lg:px-8'>
+
         <h2 className='text-xl font-bold tracking-tight text-gray-600 uppercase'>Nuestros productos</h2>
         <div className='mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-10'>
           {

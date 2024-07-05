1ca2dd8460afdf7dbc2f3ee8e3383f60f0af609a
nicolas772 <nicolas.arayau@sansano.usm.cl>
2024-07-04 20:15:23 -0400

    correccion de estilos a products y mensaje en about Us2

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,5 +1,13 @@
+import { ComputerDesktopIcon } from '@heroicons/react/24/outline'
+
 export default function AboutUs () {
   return (
-    <h1>AboutUs</h1>
+    <div id='error-page' className='flex flex-col justify-center items-center h-[70vh] w-full'>
+      <div className='flex items-center gap-2'>
+        <ComputerDesktopIcon className='h-10 text-gray-900' />
+        <h1 className='text-4xl text-bold py-7'>En construcción</h1>
+      </div>
+      <p className='pb-7'>Disculpa las molestias. Pronto podrás conocernos y saber más sobre nosotros. </p>
+    </div>
   )
 }

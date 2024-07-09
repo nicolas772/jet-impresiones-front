f08af8355e5950e90ff297f6d3de7129cc3ecefd
nicolas772 <nicolas.arayau@sansano.usm.cl>
2024-07-08 20:34:01 -0400

    loader a popular product

--- src/components/shopingCart/PopularProduct.jsx
+++ src/components/shopingCart/PopularProduct.jsx
@@ -1,22 +1,31 @@
 import { useEffect, useState } from 'react'
 import ProductService from '../../services/product.service'
 import ProductItem from '../ProductItem'
+import Loader from '../Loader'
 
 export default function PopularProduct () {
   const [popularProducts, setPopularProducts] = useState([])
+  const [loading1, setLoading1] = useState(true)
 
   useEffect(() => {
     ProductService.getAllProducts()
       .then((response) => {
         const actualPopularProducts = response.data.filter(product => product.popular)
         setPopularProducts(actualPopularProducts)
+        setLoading1(false)
       },
       (error) => {
         const _content = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
         setPopularProducts(_content)
+        setLoading1(false)
       }
       )
   }, [])
+
+  if (loading1) {
+    return <Loader />
+  }
+
   return (
     <div className='hidden xl:mt-8 xl:block'>
       <h3 className='text-lg font-bold tracking-tight text-gray-600 uppercase'>Lo más vendido</h3>
